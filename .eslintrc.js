5e1dd40665db156975f8b8c12853a58457947316
Kenshineuh <Kenshineuh@gmail.com>
2017-11-11 00:48:00 +0100

    Update to 1.2.0.
    Rework structure, add promise and functions.js, prettier, eslint

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  extends: "airbnb-base",
+  rules: {
+    quotes: ["error", "double"],
+    "no-console": [
+      "error",
+      {
+        allow: ["warn", "error", "info"],
+      },
+    ],
+    "linebreak-style": ["error", "unix"],
+    "no-underscore-dangle": "off",
+    "arrow-parens": ["error", "as-needed"],
+  },
+};
