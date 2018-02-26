695011a7f8e6222808a3b08d7808134e5d5b79fb
Enishowk <kenshineuh@gmail.com>
2018-02-26 23:04:02 +0100

    feat : rework bm command

--- src/commands/bm.js
+++ src/commands/bm.js
@@ -5,27 +5,25 @@ const config = require("../../config/config.json");
 module.exports = {
   name: "bm",
   description: "Return the BM's picture of the day.",
-  async execute(message) {
+  execute(message) {
     if (message.deletable) {
       message.delete();
     }
-    const urlPhoto = await this.getURLFromBM();
-
-    message.client.channels.get(config.nsfwChan).send(urlPhoto);
+    this.getURLFromBM().then(urlPhoto => {
+      message.client.channels.get(config.nsfwChan).send(urlPhoto);
+    });
   },
   getURLFromBM() {
-    return new Promise(resolve => {
-      axios
-        .get("http://dites.bonjourmadame.fr/")
-        .then(response => {
-          const $ = cheerio.load(response.data);
-          const urlPhoto = $(".photo.post")
-            .find("img")
-            .attr("src");
+    return axios
+      .get("http://dites.bonjourmadame.fr/")
+      .then(response => {
+        const $ = cheerio.load(response.data);
+        const urlPhoto = $(".photo.post")
+          .find("img")
+          .attr("src");
 
-          resolve(urlPhoto);
-        })
-        .catch(error => error);
-    });
+        return urlPhoto;
+      })
+      .catch(error => error);
   },
 };
