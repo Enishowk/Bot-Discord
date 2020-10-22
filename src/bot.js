ad88feef278c9da662a21053cfdebc1665f393bc
Enishowk <kenshineuh@gmail.com>
2020-10-22 13:31:37 +0200

    remove bot nvidia

--- src/bot.js
+++ src/bot.js
@@ -1,9 +1,6 @@
 require('dotenv').config()
 const Discord = require("discord.js");
 const fs = require("fs");
-const CronJob = require("cron").CronJob;
-const axios = require("axios");
-const moment = require("moment")
 
 const bot = new Discord.Client({ autoReconnect: true });
 bot.commands = new Discord.Collection();
@@ -39,29 +36,5 @@ bot.on("message", async message => {
     message.reply("there was an error trying to execute that command!");
   }
 });
-
-let lastMessage = undefined
-const checkApi = async () => {
-  try {
-    const { data } = await axios.get(
-      "https://api-prod.nvidia.com/direct-sales-shop/DR/products/fr_fr/EUR/5438795200"
-    );
-    data.products.product.forEach((element) => {
-      if (element.inventoryStatus.productIsInStock === "true" && element.inventoryStatus.status !== "PRODUCT_INVENTORY_OUT_OF_STOCK") {
-        console.log(lastMessage && lastMessage.format("hh:mm:ss"))
-        if (lastMessage === undefined || moment().isAfter(lastMessage.clone().add(10, 'm'))) {
-          lastMessage = moment();
-          bot.users.fetch(process.env.USER_ADMIN).then(user => user.send("https://www.nvidia.com/fr-fr/geforce/graphics-cards/30-series/rtx-3080/ !!"))
-        }
-      }
-    });
-  } catch (error) {
-    console.error("ERROR CRON:", error.message)
-  }
-};
-const job = new CronJob("0 * * * * *", async function () {
-  checkApi();
-});
-job.start();
 
 bot.login(process.env.TOKEN);
